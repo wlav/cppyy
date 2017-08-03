be5abec9aec9c4471899408e0f457585351c0846
Wim Lavrijsen <WLavrijsen@lbl.gov>
2017-08-03 11:43:24 -0700

    cleanup

--- python/cppyy/_cpython_cppyy.py
+++ python/cppyy/_cpython_cppyy.py
@@ -11,53 +11,15 @@ __all__ = [
     '_backend',
     ]
 
-# first load the dependency libraries of the backend, then
-# pull in the libcppyy extension module
+# first load the dependency libraries of the backend, then pull in the
+# libcppyy extension module
 c = loader.load_cpp_backend()
 import libcppyy as _backend
 _backend._cpp_backend = c
 
-# lookups on namespaces
-# TODO: why here, and if so, how to share with PyPy/_cppyy?
 
-### -----------------------------------------------------------------------------
-### -- metaclass helper from six ------------------------------------------------
-### -- https://bitbucket.org/gutworth/six/src/8a545f4e906f6f479a6eb8837f31d03731597687/six.py?at=default#cl-800
-#
-# Copyright (c) 2010-2015 Benjamin Peterson
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-def with_metaclass(meta, *bases):
-    """Create a base class with a metaclass."""
-    # This requires a bit of explanation: the basic idea is to make a dummy
-    # metaclass for one level of class instantiation that replaces itself with
-    # the actual metaclass.
-    class metaclass(meta):
-        def __new__(cls, name, this_bases, d):
-            return meta(name, bases, d)
-    return type.__new__(metaclass, 'temporary_class', (), {})
-
-
-### template support ------------------------------------------------------------
-# TODO: why is this necessary?? And if so, can it be shared with PyPy/_cppyy?
-class Template:
+### template support ---------------------------------------------------------
+class Template(object):  # expected/used by CPyCppyyHelpers.cxx in CPyCppyy
     def __init__(self, name):
         self.__name__ = name
 
@@ -102,23 +64,17 @@ class _ns_meta(type):
         setattr(cls, name, attr)
         return attr
 
-class _stdmeta(type):
-    def __getattr__(cls, name):     # for non-templated classes in std
-        try:
-          # TODO: why only classes here?
-            klass = _backend.CreateScopeProxy(name, cls)
-        except TypeError as e:
-            raise AttributeError(str(e))
-        setattr( cls, name, klass )
-        return klass
+class gbl(object):
+    __metaclass__ = _ns_meta
 
+    class std(object):
+        __metaclass__ = _ns_meta
 
-#- exports -------------------------------------------------------------------
-class gbl(with_metaclass(_ns_meta)):
-    class std(with_metaclass(_stdmeta, object)):
       # pre-get string to ensure disambiguation from python string module
         string = _backend.CreateScopeProxy('string')
 
+
+#- exports -------------------------------------------------------------------
 addressof = _backend.addressof
 bind_object = _backend.bind_object
 
