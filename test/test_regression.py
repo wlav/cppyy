5ad8def8fc23b751d956da16f87231e94a0318c4
Wim Lavrijsen <WLavrijsen@lbl.gov>
2017-10-25 20:44:31 -0700

    improve and expand the pydoc regression test

--- test/test_regression.py
+++ test/test_regression.py
@@ -6,13 +6,21 @@ currpath = py.path.local(__file__).dirpath()
 
 
 class TestREGRESSION:
+    helpout = []
+
     def setup_class(cls):
         import cppyy
+
+        def stringpager(text, cls=cls):
+            cls.helpout.append(text)
+
+        import pydoc
+        pydoc.pager = stringpager
 
     def test01_kdcraw(self):
         """Template member functions lookup and calls"""
 
-        import cppyy
+        import cppyy, pydoc
 
         # TODO: run a find for these paths
         qtpath = "/usr/include/qt5"
@@ -22,15 +30,41 @@ class TestREGRESSION:
             warnings.warn("no KDE/Qt found, skipping test01_kdcraw")
             return
 
+        # need to resolve qt_version_tag for the incremental compiler; since
+        # it's not otherwise used, just make something up
+        cppyy.cppdef("int qt_version_tag = 42;")
         cppyy.add_include_path(qtpath)
         cppyy.include(kdcraw_h)
 
         from cppyy.gbl import KDcrawIface
-        help(KDcrawIface)
+
+        self.__class__.helpout = []
+        pydoc.doc(KDcrawIface)
+        helptext  = ''.join(self.__class__.helpout)
+        assert 'KDcrawIface' in helptext
 
     def test02_dir(self):
         """For the same reasons as test01_kdcraw, this used to crash."""
 
-        import cppyy
+        import cppyy, pydoc
 
-        help(cppyy.gbl.gInterpreter)
+        assert not '__abstractmethods__' in dir(cppyy.gbl.gInterpreter)
+        assert '__class__' in dir(cppyy.gbl.gInterpreter)
+
+        self.__class__.helpout = []
+        pydoc.doc(cppyy.gbl.gInterpreter)
+        helptext = ''.join(self.__class__.helpout)
+        assert 'TInterpreter' in helptext
+        assert 'ObjectProxy' in helptext
+        assert 'AddIncludePath' in helptext
+
+        cppyy.cppdef("namespace cppyy_regression_test { int iii = 42; }")
+
+        assert not '__abstractmethods__' in dir(cppyy.gbl.cppyy_regression_test)
+        assert '__class__' in dir(cppyy.gbl.cppyy_regression_test)
+        assert 'iii' in dir(cppyy.gbl.cppyy_regression_test)
+
+        self.__class__.helpout = []
+        pydoc.doc(cppyy.gbl.cppyy_regression_test)
+        helptext = ''.join(self.__class__.helpout)
+        assert 'ObjectProxy' in helptext
