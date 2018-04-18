a4f70d095df287f69d3728ba721e0752fe029026
Wim Lavrijsen <WLavrijsen@lbl.gov>
2018-04-18 09:09:07 -0700

    more tests of documentation

--- test/test_doc_features.py
+++ test/test_doc_features.py
@@ -7,24 +7,38 @@ class TestDOCFEATURES:
         import cppyy
 
         cppyy.cppdef("""
+#include <cmath>
 #include <iostream>
 #include <vector>
 
+
+//-----
 unsigned int gUint = 0;
 
-class AbstractClass {
+//-----
+class Abstract {
 public:
-    virtual ~AbstractClass() {}
+    virtual ~Abstract() {}
     virtual void abstract_method() = 0;
+    virtual void concrete_method() = 0;
 };
 
-class ConcreteClass : AbstractClass {
+void Abstract::concrete_method() {
+    std::cout << "called Abstract::concrete_method" << std::endl;
+}
+
+//-----
+class Concrete : Abstract {
 public:
-    ConcreteClass(int n=42) : m_int(n), m_const_int(17) {}
-    ~ConcreteClass() {}
+    Concrete(int n=42) : m_int(n), m_const_int(17) {}
+    ~Concrete() {}
 
     virtual void abstract_method() {
-        std::cout << "called concrete method" << std::endl;
+        std::cout << "called Concrete::abstract_method" << std::endl;
+    }
+
+    virtual void concrete_method() {
+        std::cout << "called Concrete::concrete_method" << std::endl;
     }
 
     void array_method(int* ad, int size) {
@@ -39,7 +53,7 @@ public:
         std::cout << std::endl;
     }
 
-    AbstractClass* show_autocast() {
+    Abstract* show_autocast() {
         return this;
     }
 
@@ -53,9 +67,19 @@ public:
     const int m_const_int;
 };
 
+//-----
+int global_function(int) {
+    return 42;
+}
+
+double global_function(double) {
+    return std::exp(1);
+}
+
+//-----
 namespace Namespace {
 
-    class ConcreteClass {
+    class Concrete {
     public:
         class NestedClass {
         public:
@@ -64,24 +88,33 @@ namespace Namespace {
 
     };
 
+    int global_function(int i) {
+        return 2*::global_function(i);
+    }
+
+    double global_function(double d) {
+        return 2*::global_function(d);
+    }
+
 } // namespace Namespace
+
 """)
 
     def test_abstract_class(self):
         import cppyy
-        from cppyy.gbl import AbstractClass, ConcreteClass
+        from cppyy.gbl import Abstract, Concrete
 
-        raises(TypeError, AbstractClass)
-        assert issubclass(ConcreteClass, AbstractClass)
-        c = ConcreteClass()
-        assert isinstance(c, AbstractClass)
+        raises(TypeError, Abstract)
+        assert issubclass(Concrete, Abstract)
+        c = Concrete()
+        assert isinstance(c, Abstract)
 
     def test_array(self):
         import cppyy
-        from cppyy.gbl import ConcreteClass
+        from cppyy.gbl import Concrete
         from array import array
 
-        c = ConcreteClass()
+        c = Concrete()
         c.array_method(array('d', [1., 2., 3., 4.]), 4)
         raises(IndexError, c.m_data.__getitem__, 4)
 
@@ -93,50 +126,50 @@ namespace Namespace {
         
     def test_casting(self):
         import cppyy
-        from cppyy.gbl import AbstractClass, ConcreteClass
+        from cppyy.gbl import Abstract, Concrete
 
-        c = ConcreteClass()
-        assert 'AbstractClass' in ConcreteClass.show_autocast.__doc__
+        c = Concrete()
+        assert 'Abstract' in Concrete.show_autocast.__doc__
         d = c.show_autocast()
-        assert type(d) == cppyy.gbl.ConcreteClass
+        assert type(d) == cppyy.gbl.Concrete
 
         from cppyy import addressof, bind_object
-        e = bind_object(addressof(d), AbstractClass)
-        assert type(e) == cppyy.gbl.AbstractClass
+        e = bind_object(addressof(d), Abstract)
+        assert type(e) == cppyy.gbl.Abstract
 
     def test_classes_and_structs(self):
         import cppyy
-        from cppyy.gbl import ConcreteClass, Namespace
+        from cppyy.gbl import Concrete, Namespace
 
-        assert ConcreteClass != Namespace.ConcreteClass
-        n = Namespace.ConcreteClass.NestedClass()
-        assert 'Namespace.ConcreteClass.NestedClass' in str(type(n))
+        assert Concrete != Namespace.Concrete
+        n = Namespace.Concrete.NestedClass()
+        assert 'Namespace.Concrete.NestedClass' in str(type(n))
         assert 'NestedClass' == type(n).__name__
-        assert 'cppyy.gbl.Namespace.ConcreteClass' == type(n).__module__
-        assert 'Namespace::ConcreteClass::NestedClass' == type(n).__cppname__
+        assert 'cppyy.gbl.Namespace.Concrete' == type(n).__module__
+        assert 'Namespace::Concrete::NestedClass' == type(n).__cppname__
 
     def test_data_members(self):
         import cppyy
-        from cppyy.gbl import ConcreteClass
+        from cppyy.gbl import Concrete
 
-        c = ConcreteClass()
+        c = Concrete()
         assert c.m_int == 42
         raises(TypeError, setattr, c, 'm_const_int', 71)
 
     def test_default_arguments(self):
         import cppyy
-        from cppyy.gbl import ConcreteClass
+        from cppyy.gbl import Concrete
 
-        c = ConcreteClass()
+        c = Concrete()
         assert c.m_int == 42
-        c = ConcreteClass(13)
+        c = Concrete(13)
         assert c.m_int == 13
 
     def test_doc_strings(self):
         import cppyy
-        from cppyy.gbl import ConcreteClass
-        assert 'void ConcreteClass::array_method(int* ad, int size)' in ConcreteClass.array_method.__doc__
-        assert 'void ConcreteClass::array_method(double* ad, int size)' in ConcreteClass.array_method.__doc__
+        from cppyy.gbl import Concrete
+        assert 'void Concrete::array_method(int* ad, int size)' in Concrete.array_method.__doc__
+        assert 'void Concrete::array_method(double* ad, int size)' in Concrete.array_method.__doc__
 
     def test_enums(self):
         import cppyy
@@ -146,7 +179,16 @@ namespace Namespace {
     def test_functions(self):
         import cppyy
 
-        pass
+        from cppyy.gbl import global_function, Namespace
+        assert not(global_function == Namespace.global_function)
+
+        assert round(global_function(1.)-2.718281828459045, 8) == 0.
+        assert global_function(1) == 42
+
+        assert Namespace.global_function(1) == 42*2
+        assert round(Namespace.global_function(1.)-2.718281828459045*2, 8) == 0.
+
+        assert round(global_function.__overload__('double')(1)-2.718281828459045, 8) == 0.
 
     def test_inheritance(self):
         import cppyy
@@ -155,9 +197,9 @@ namespace Namespace {
 
     def test_memory(self):
         import cppyy
-        from cppyy.gbl import ConcreteClass
+        from cppyy.gbl import Concrete
 
-        c = ConcreteClass()
+        c = Concrete()
         assert c.__python_owns__ == True
 
     def test_methods(self):
@@ -178,9 +220,9 @@ namespace Namespace {
 
     def test_operator_conversions(self):
         import cppyy
-        from cppyy.gbl import ConcreteClass
+        from cppyy.gbl import Concrete
 
-        assert str(ConcreteClass()) == 'Hello operator const char*!'
+        assert str(Concrete()) == 'Hello operator const char*!'
 
     def test_operator_overloads(self):
         import cppyy
