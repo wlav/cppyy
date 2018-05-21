37be2e99f98c0692b61d788b0d5acaff2bdd3db0
Wim Lavrijsen <WLavrijsen@lbl.gov>
2018-05-21 14:44:47 -0700

    improved pythonization tests

--- test/pythonizables.cxx
+++ test/pythonizables.cxx
@@ -43,10 +43,10 @@ pyzables::SharedCountable_t pyzables::gime_mine() { return mine; }
 pyzables::SharedCountable_t* pyzables::gime_mine_ptr() { return &mine; }
 pyzables::SharedCountable_t& pyzables::gime_mine_ref() { return mine; }
 
-unsigned long pyzables::pass_mine_sp(std::shared_ptr<Countable> ptr) { return ptr->m_check; }
-unsigned long pyzables::pass_mine_sp_ref(std::shared_ptr<Countable>& ptr) { return ptr->m_check; }
-unsigned long pyzables::pass_mine_sp_ptr(std::shared_ptr<Countable>* ptr) { return (*ptr)->m_check; }
+unsigned int pyzables::pass_mine_sp(std::shared_ptr<Countable> ptr) { return ptr->m_check; }
+unsigned int pyzables::pass_mine_sp_ref(std::shared_ptr<Countable>& ptr) { return ptr->m_check; }
+unsigned int pyzables::pass_mine_sp_ptr(std::shared_ptr<Countable>* ptr) { return (*ptr)->m_check; }
 
-unsigned long pyzables::pass_mine_rp(Countable c) { return c.m_check; }
-unsigned long pyzables::pass_mine_rp_ref(const Countable& c) { return c.m_check; }
-unsigned long pyzables::pass_mine_rp_ptr(const Countable* c) { return c->m_check; }
+unsigned int pyzables::pass_mine_rp(Countable c) { return c.m_check; }
+unsigned int pyzables::pass_mine_rp_ref(const Countable& c) { return c.m_check; }
+unsigned int pyzables::pass_mine_rp_ptr(const Countable* c) { return c->m_check; }
