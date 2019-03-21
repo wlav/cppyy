bbdff24378a0ba14749a4f99f31757981d731a7f
Wim Lavrijsen <WLavrijsen@lbl.gov>
2019-03-20 22:08:01 -0700

    exceptions can now propagate 1 layer on Windows

--- test/test_crossinheritance.py
+++ test/test_crossinheritance.py
@@ -198,10 +198,6 @@ class TestCROSSINHERITANCE:
                 raise ValueError(errmsg)
 
         d = Derived()
-
-        if 'win32' in sys.platform:
-            import pytest
-            pytest.skip('C++ exceptions do not properly propagate')
         raises(ValueError, Base1.call_sum_value, d, -7)
 
         try:
