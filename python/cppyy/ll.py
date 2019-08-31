4a747df3eac14366d7e78d209797b2604d5dc4e4
Wim Lavrijsen <WLavrijsen@lbl.gov>
2019-08-31 16:51:10 -0700

    expand low-level functions

--- python/cppyy/ll.py
+++ python/cppyy/ll.py
@@ -12,8 +12,9 @@ __all__ = [
     ]
 
 
-# create low-level casting helpers
+# create low-level helpers
 cppyy.cppdef("""namespace __cppyy_internal {
+// type casting
     template<typename T, typename U>
     T cppyy_cast(U val) { return (T)val; }
 
@@ -25,11 +26,39 @@ cppyy.cppdef("""namespace __cppyy_internal {
 
     template<typename T, typename S>
     T* cppyy_dynamic_cast(S* obj) { return dynamic_cast<T*>(obj); }
+
+// memory allocation/free-ing
+    template<typename T>
+    T* cppyy_malloc(size_t count=1) { return (T*)malloc(sizeof(T*)*count); }
+
+    template<typename T>
+    T* cppyy_array_new(size_t count) { return new T[count]; }
+
+    template<typename T>
+    void cppyy_array_delete(T* ptr) { delete[] ptr; }
 }""")
 
+
+# helper for sizing arrays
+class ArraySizer(object):
+    def __init__(self, func):
+        self.func = func
+    def __getitem__(self, t):
+        self.array_type = t
+        return self
+    def __call__(self, size):
+        res = self.func[self.array_type](size)
+        res.reshape((size,))
+        return res
 
 # import casting helpers
 cast             = cppyy.gbl.__cppyy_internal.cppyy_cast
 static_cast      = cppyy.gbl.__cppyy_internal.cppyy_static_cast
 reinterpret_cast = cppyy.gbl.__cppyy_internal.cppyy_reinterpret_cast
 dynamic_cast     = cppyy.gbl.__cppyy_internal.cppyy_dynamic_cast
+
+# import memory allocation/free-ing helpers
+malloc           = ArraySizer(cppyy.gbl.__cppyy_internal.cppyy_malloc)
+free             = cppyy.gbl.free      # for symmetry
+array_new        = ArraySizer(cppyy.gbl.__cppyy_internal.cppyy_array_new)
+array_delete     = cppyy.gbl.__cppyy_internal.cppyy_array_delete
