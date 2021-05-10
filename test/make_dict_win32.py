3c4b13876a66f0e9a0008689d0fe6169f020b9fd
Wim Lavrijsen <WLavrijsen@lbl.gov>
2021-05-09 17:23:17 -0700

    remove spurious space

--- test/make_dict_win32.py
+++ test/make_dict_win32.py
@@ -39,7 +39,7 @@ if os.path.exists(fn+'Dict.dll'):
         os.remove(fg)
 
 def _get_config_exec():
-        return [sys.executable, '-m', 'cppyy_backend._cling_config']
+    return [sys.executable, '-m', 'cppyy_backend._cling_config']
 
 def get_config(what):
     config_exec_args = _get_config_exec()
