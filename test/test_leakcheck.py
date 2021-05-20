c246b178f360e3e61c7035464ee995fc1d7c4fcb
Wim Lavrijsen <WLavrijsen@lbl.gov>
2021-05-19 22:25:08 -0700

    basic whole-method leak checker

---
+++ test/test_leakcheck.py
@@ -0,0 +1,87 @@
+import py, os, sys
+from pytest import mark
+from .support import setup_make, pylong, pyunicode
+
+currpath = py.path.local(__file__).dirpath()
+test_dct = str(currpath.join("datatypesDict"))
+
+def setup_module(mod):
+    setup_make("datatypes")
+
+nopsutil = False
+try:
+    import psutil
+except Exception:
+    nopsutil = True
+
+
+@mark.skipif(nopsutil == True, reason="module psutil not installed")
+class TestLEAKCHECK:
+    def setup_class(cls):
+        import cppyy, psutil
+
+        cls.test_dct = test_dct
+        cls.memory = cppyy.load_reflection_info(cls.test_dct)
+
+        cls.process = psutil.Process(os.getpid())
+
+    def check_func(self, scope, func, *args, **kwds):
+        """Leak-check 'func', given args and kwds"""
+
+      # warmup (once should suffice in all cases)
+        getattr(scope, func)(*args, **kwds)
+
+      # leak check
+        last = self.process.memory_info().rss
+
+        for i in range(100000):
+            getattr(scope, func)(*args, **kwds)
+
+        assert last == self.process.memory_info().rss
+
+    def test01_free_functions(self):
+        """Leak test of free functions"""
+
+        import cppyy
+
+        cppyy.cppdef("""\
+        namespace LeakCheck {
+        void free_f() {}
+        void free_f_ol(int) {}
+        void free_f_ol(std::string s) {}
+        std::string free_f_ret() { return "aap"; }
+        template<class T> void free_f_ol(T) {}
+        }""")
+
+        ns = cppyy.gbl.LeakCheck
+
+        self.check_func(ns, 'free_f')
+        self.check_func(ns, 'free_f_ol', 42)
+        self.check_func(ns, 'free_f_ol', '42')
+        self.check_func(ns, 'free_f_ol', 42.)
+        self.check_func(ns, 'free_f_ret')
+
+    def test02_test_methods(self):
+        """Leak test of methods"""
+
+        import cppyy
+
+        cppyy.cppdef("""\
+        namespace LeakCheck {
+        class MyClass {
+        public:
+            void method() {}
+            void method_ol(int) {}
+            void method_ol(std::string s) {}
+            std::string method_ret() { return "aap"; }
+            template<class T> void method_ol(T) {}
+        }; }""")
+
+        ns = cppyy.gbl.LeakCheck
+
+        m = ns.MyClass()
+        self.check_func(m, 'method')
+        self.check_func(m, 'method_ol', 42)
+        self.check_func(m, 'method_ol', '42')
+        self.check_func(m, 'method_ol', 42.)
+        self.check_func(m, 'method_ret')
