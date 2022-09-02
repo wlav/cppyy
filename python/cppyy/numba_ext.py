09d3426dc743d936f53066512551eeb2ca838d27
Baidyanath Kundu <kundubaidya99@gmail.com>
2022-09-03 01:11:18 +0200

    Enable use of the new overload for FindOverload

--- python/cppyy/numba_ext.py
+++ python/cppyy/numba_ext.py
@@ -130,7 +130,7 @@ class CppFunctionNumbaType(nb_types.Callable):
         except KeyError:
             pass
 
-        ol = CppFunctionNumbaType(self._func.__overload__(*(numba2cpp(x) for x in args)), self._is_method)
+        ol = CppFunctionNumbaType(self._func.__overload__(tuple(numba2cpp(x) for x in args)), self._is_method)
 
         thistype = None
         if self._is_method:
@@ -168,7 +168,7 @@ class CppFunctionNumbaType(nb_types.Callable):
 
     def get_pointer(self, func):
         if func is None: func = self._func
-        ol = func.__overload__(*(numba2cpp(x) for x in self.sig.args))
+        ol = func.__overload__(tuple(numba2cpp(x) for x in self.sig.args))
         address = cppyy.addressof(ol)
         if not address:
             raise RuntimeError("unresolved address for %s" % str(ol))
